279f7593cc7dc5bb79309da05af0303228cf42df
Jim Wooley <jimwooley@hotmail.com>
2017-02-21 23:14:22 -0500

    Update for VS 2017RC3 Include C#7 sample project

---
+++ LanguageFeaturesCs7/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LanguageFeaturesCs7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LanguageFeaturesCs7")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c642fd5e-d8b6-4535-9869-7c9d078d6449")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LanguageFeaturesCs7/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LanguageFeaturesCs7
+{
+    [TestClass]
+    public class CS7Tests
+    {
+        [TestMethod]
+        public void CS7Test()
+        {
+            object[] numbers = { 0b1, 0b10, new object[] { 0b10, 0b100, 0b1000 }, 0b1000_0, 0b1000_00 };
+            (int sum, int count) Tally(IEnumerable<object> list)
+            {
+                var r = (sum: 0, count: 0);
+                foreach (var v in list)
+                {
+                    switch (v)
+                    {
+                        case int i:
+                            r.sum += i;
+                            r.count++;
+                            break;
+                        case IEnumerable<object> l when l.Any():
+                            var t1 = Tally(l);
+                            r.sum += t1.sum;
+                            r.count += t1.count;
+                            break;
+                        case null:
+                            break;
+                    }
+                }
+                return r;
+            }
+            var result = Tally(numbers);
+            Trace.WriteLine($"Sum: {result.sum} Count: {result.count}");
+
+        }
+    }
+}
